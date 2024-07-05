74c1d15198604d88ee0c0113f674566f6e0d5f7b
Youssef-faradi <faradiyoussef1@gmail.com>
2024-07-05 20:27:38 +0100

    dads

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Contains the top_ten function"""
+""" prints the titles of the first 10 hot posts """
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10
-    hot posts listed for a given subreddit"""
-    url = f"https://api.reddit.com/r/{subreddit}?sort=hot&limit=9"
-    headers = {'User-Agent': 'MyClient/1.0'}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "custom"}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print('None')
-        exit()
-    subreddits = response.json()
-    children = subreddits['data']['children']
-    [print(child['data']['title']) for child in children]
-    
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        posts = response.json()["data"]["children"]
+        for post in posts:
+            print(post["data"]["title"])
+    else:
+        print(None)
