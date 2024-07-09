91ecc6bf37c3ae9cae91f3ece34fad0279f47099
Youssef-faradi <faradiyoussef1@gmail.com>
2024-07-09 09:56:49 +0000

    update 0x1B-web_stack_debugging_4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,11 @@
-# Fix problem of high amount of requests
-
-exec {'replace':
-  provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart'],
+# Fix the nginx requests limit
+exec { 'upgrade':
+  path    => '/bin/',
+  command => 'sed -i "s/15/4096" /etc/default/nginx',
 }
 
-exec {'restart':
-  provider => shell,
-  command  => 'sudo service nginx restart',
+exec { 'restart':
+  path    => '/usr/bin/',
+  command => 'service nginx restart',
 }
+
